5ef63b1ea9934087a4b59a4f2b5a48c839eeb162
dmitriwd <dmitri.net@protonmail.com>
2020-11-03 12:49:53 +0100

    Some bugs to display obstacles

--- Player.js
+++ Player.js
@@ -6,23 +6,6 @@ class Player{
         this.height = 222;
     };
 
-    collisioncheck(obstacle){
-        const playersRightSide = this.x + this.width;
-        const obstaclesRightSide = obstacle.x + obstacle.width;
-        const playersbottomSide = this.y + this.height;
-        const obstaclesBottomSide = obstacle.y + obstacle.height;
-
-        const touchingLeft = playersRightSide >= obstacle.x;
-        const touchingRight = this.x <= obstaclesRightSide;
-        const touchingTop = playersbottomSide >= obstacle.height;
-        const touchingBottom = this.y <= obstaclesBottomSide;
-    
-    if (touchingBottom && touchingLeft && touchingRight && touchingTop){
-        return true;
-    };
-    return false;
-    };
-
     moveRight(steps) {
         if (this.x >= 900 - this.width) {
           return;
